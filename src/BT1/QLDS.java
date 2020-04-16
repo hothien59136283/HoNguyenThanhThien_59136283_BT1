7cb8a15415f620658e452ac3ff1e9b0013cffa36
HoNguyenThanhThien <vietanhchannel@gmail.com>
2020-04-16 21:15:24 +0700

    v1.0

---
+++ src/BT1/QLDS.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BT1;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author ltpnt
+ */
+public class QLDS implements IQLDS{
+    private List<CaNhan> dsCaNhan = new ArrayList<>();
+
+    @Override
+    public int them(CaNhan p) {
+        dsCaNhan.add(p);
+        return 0;
+    }
+
+    @Override
+    public int xoa(String ten) {
+        for(CaNhan caNhan: dsCaNhan) {
+            if(caNhan.getHoTen().equals(ten)) {
+                dsCaNhan.remove(caNhan);
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public void inDS() {
+        for(CaNhan caNhan: dsCaNhan) {
+           caNhan.hienThiTT();
+       }
+    }
+        
+}
